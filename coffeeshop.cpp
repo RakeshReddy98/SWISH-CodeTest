70405d88503550270caaf38a57504c3993f77506
RakeshReddy98 <53935808+RakeshReddy98@users.noreply.github.com>
2019-08-09 14:39:27 +0530

    coffeeshop.cpp

---
+++ coffeeshop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cin>>a>>b;
+if(a==0)
+cout<<"0"<<endl;
+else{
+int cnt =a;
+while(x>0){
+float k = (float)(a*b)/100;
+a -= k;
+cnt += a;
+}
+cout<<cnt<<endl;
+}
+return 0;
+}
